645f558761fd7435c1628bb5fd28112eeeefca13
Caio Boris <caiogamer2000@gmail.com>
2020-03-10 17:29:16 -0300

    Resoluçõa exercicio 1

---
+++ ConsoleApp5/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace ConsoleApp5
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			string[] nomes =
+			{
+				"Fabio Junior",
+				"Fiuk",
+				"Cleo Pires",
+				"Gloria pires"
+
+			};
+
+			foreach(var nome in nomes)
+			{
+				Console.WriteLine($"O nome é  {nome}");
+			}
+
+			Console.ReadLine();
+		}
+	}
+}
